66bafed30ac49368caec448d165438c4d3394fd9
amanpreet1998-frontend <amandhaliwal1693@gmail.com>
2025-05-27 22:00:35 +0530

    Update PlaceOrder.jsx

--- src/components/PlaceOrder.jsx
+++ src/components/PlaceOrder.jsx
@@ -4,7 +4,7 @@ import '../components/style/PlaceOrder.css';
 import { useSelector } from 'react-redux';
 import { toast, ToastContainer } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
-import { clearCart } from '../redux/cartSlice';/*change*/
+import { clearCart } from '../redux/CartSlice';/*change*/
 import { useDispatch } from 'react-redux';/*change*/
 
 
